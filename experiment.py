1d78725aea72c485a5c68e64f76eb46ca6201860
srutownik <marek.28.93@gmail.com>
2018-05-20 18:38:54 +0200

    checkpoints

--- experiment.py
+++ experiment.py
@@ -1,14 +1,16 @@
 from simulation.environment import Environment
 from gen.logging import Stats
+from gen.checkpoints import CheckpointManager
 
 class Experiment:
     def __init__(self, toolbox, epochs=50, checkpoint=None):
         self.environment = Environment()
+        self.checkpoints = CheckpointManager(checkpoint)
         self.setup_toolbox(toolbox)
         self.epochs = epochs
 
-        if checkpoint:
-            self.load_state(checkpoint)
+        if self.checkpoints.last_epoch >= 0:
+            self.load_state()
         else:
             self.init_state()
 
@@ -16,7 +18,7 @@ class Experiment:
         self.toolbox = toolbox
         self.stats = Stats()
         self.toolbox.register('run_simulation', self.environment.run_simulation)
-        self.toolbox.decorate('run_simulation', self.stats.log_decorator)
+        self.toolbox.decorate('run_simulation', self.stats.log_decorator, self.checkpoints.save_decorator)
 
     def init_state(self):
         self.iteration = 0
@@ -24,8 +26,11 @@ class Experiment:
         population, = self.toolbox.population()
         self.population, = self.toolbox.run_simulation(population, self.random_seed())
 
-    def load_state(self, checkpoint):
-        pass
+    def load_state(self):
+        self.iteration   = self.checkpoints.last_epoch
+        population, seed = self.checkpoints.load_epoch(self.iteration)
+
+        self.population, = self.toolbox.run_simulation(population, seed)
 
     def run(self):
         while not self.finished():
---
+++ gen/checkpoints.py
@@ -0,0 +1,34 @@
+import os
+import glob
+import pickle
+import datetime
+
+class CheckpointManager:
+    def __init__(self, checkpoint_dir):
+        if checkpoint_dir is None:
+            checkpoint_dir = os.path.join('checkpoints', str(datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')))
+
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+
+        self.checkpoint_dir = checkpoint_dir
+        self.last_epoch = int(os.path.basename(max(glob.glob(os.path.join(checkpoint_dir, '*.pkl')),
+                                                   default='-1.pkl'))[:-4])
+
+    def save(self, population, seed):
+        self.last_epoch += 1
+
+        with open(os.path.join(self.checkpoint_dir, '%d.pkl' % self.last_epoch), 'wb') as f:
+            pickle.dump([population, seed], f)
+
+    def save_decorator(self, foo):
+        def wrapper(population, seed, *args, **kwargs):
+            self.save(population, seed)
+
+            return foo(population, seed, *args, **kwargs)
+
+        return wrapper
+
+    def load_epoch(self, epoch):
+        with open(os.path.join(self.checkpoint_dir, '%d.pkl' % epoch), 'rb') as f:
+            return pickle.load(f)
